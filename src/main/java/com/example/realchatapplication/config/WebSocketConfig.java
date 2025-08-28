ffd34d4ff385da8a49a3a8f9d5b4e309fee6b8b3
Pradeep-Chandra-17 <pradeepchandragajendra@gmail.com>
2025-08-28 16:35:11 +0530

    Fixed local frontend api to be allowed origin pattern in WebSocketConfig

--- src/main/java/com/example/realchatapplication/config/WebSocketConfig.java
+++ src/main/java/com/example/realchatapplication/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.example.realchatapplication.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${frontend.url}")
+    private String FRONTEND_URL;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -23,7 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry.addEndpoint("/ws").
-                setAllowedOriginPatterns("http://localhost:5173", "http://localhost:3000")
+                setAllowedOriginPatterns("http://localhost:5173", FRONTEND_URL)
                 .withSockJS();
     }
 }
